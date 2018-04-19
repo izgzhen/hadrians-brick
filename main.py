9d6879348fbf62cca718d352529100c16989e7c6
Zhen Zhang <zhen@pineapple.exchange>
2018-04-19 15:06:50 +0800

    check in base

---
+++ main.py
@@ -0,0 +1,64 @@
+import re
+import os
+import time
+import json
+import platform
+import subprocess
+
+from base import argparse_, subprocess_
+from base.assert_ import *
+
+parser = argparse_.p()
+parser.add_argument('--ghc_path')
+args = parser.parse_args()
+
+assert args.ghc_path, '--ghc_path is required'
+
+hadrian_path = args.ghc_path + '/hadrian'
+
+# TODO(zhen): validate path here
+
+def get_gcc_version():
+    code, stdout, stderr = subprocess_.call_std(['gcc', '-v'])
+    assert_eq(code, 0)
+    m = re.search('gcc version ([\d\.]+)', stderr)
+    return m.group(1)
+
+def get_ghc_version():
+    code, stdout, stderr = subprocess_.call_std(['ghc', '--numeric-version'])
+    assert_eq(code, 0)
+    return stdout.strip()
+
+def get_git_hash(path):
+    code, stdout, stderr = subprocess_.call_std(['git', 'rev-parse', 'HEAD'], cwd=path)
+    assert_eq(code, 0)
+    return stdout.strip()
+
+def get_build_info():
+    info = {}
+    info['platform'] = platform.platform()
+    info['stage0-ghc-version'] = get_ghc_version()
+    info['gcc-version'] = get_gcc_version()
+    info['ghc-git-hash'] = get_git_hash(args.ghc_path)
+    info['hadrian-git-hash'] = get_git_hash(hadrian_path)
+    info['timestamp'] = time.ctime()
+    return info
+
+def run_build():
+    now = time.time()
+    code = subprocess.call(['bash', 'build.sh', '--flavour=quickest'], cwd=hadrian_path)
+    duration = time.time() - now
+    info = get_build_info()
+    info['duration'] = duration
+    info['exit-code'] = code
+    print("================= SUMMARY =================")
+    print(info)
+    home_dir = os.getenv('HOME')
+    os.mkdir(home_dir + '/.hadrians-wall')
+    f = open(home_dir + '/.hadrians-wall/build-info.log', 'a+')
+    dumped = json.dumps(info)
+    assert '\n' not in dumped, 'newline in JSON export'
+    f.write(dumped + '\n')
+    f.close()
+
+run_build()
